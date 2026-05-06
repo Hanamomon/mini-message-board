c4bfe207e13e577acb65d5dd830db5a5a7c68eb9
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-05-06 16:25:17 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+const indexRouter = require('./routes/indexRouter');
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(3000);
