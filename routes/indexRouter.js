1fde937e64a9b3f07cd1444b164887b1a8a4eea3
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-05-06 17:12:15 -0700

    feat: add single message view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,11 +4,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -27,9 +29,15 @@ indexRouter.post('/new', (req, res) => {
   const messageText = req.body.message;
   const messageAuthor = req.body.author;
 
-  messages.push({ text: messageText, user: messageAuthor, added: new Date() });
+  messages.push({ id: messages.length()++ ,text: messageText, user: messageAuthor, added: new Date() });
 
   res.redirect("/");
+})
+
+indexRouter.get('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const message = messages.find(message => message.id === id);
+  res.render('message', { message: message });
 })
 
 module.exports = indexRouter;
